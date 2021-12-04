a7b1051224217518ed5c40268f0e0fe396c47199
JoseJare <89498260+JoseJare@users.noreply.github.com>
2021-12-03 20:50:31 -0600

    Add files via upload

---
+++ AngryBirdEtapa5-main/Script.js
@@ -0,0 +1,27 @@
+//cadena
+var string = "this.is.a.chain";
+console.log(string);
+
+//numero
+var number = 100;
+console.log(number);
+//como son los boleanos
+var bool = true
+console.log(bool);
+//indefinido
+var object;
+console.log(object);
+//nulo
+object = nulo
+console.log(object);
+//uso de matrices
+var matric1 =[1, 2, 3, 4, 5];
+console.log(matric1);
+//visualisar un elemento de una matric
+console.log(matric1[1]);
+var matric2 = ["Jared", 13, true]
+console.log(matric2);
+//una matric que almacena matrices 
+var matric3 =[[1, 2],[3, 4],[5, 6]]
+console.log(matric3);
+console.log(matric3[0][0]);
